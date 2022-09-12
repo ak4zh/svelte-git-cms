c8c5319fcbb1794b9035700e50a93b15738deb9c
Ak4zh <agwl.akash@gmail.com>
2022-09-12 15:22:45 +0530

    improved slugify

--- src/lib/index.js
+++ src/lib/index.js
@@ -104,16 +104,16 @@ function labelToTag(label, label_prefix) {
  * @param {string | number} text
  * @returns {string}
  */
-function slugify(text) {
+ function slugify(text) {
     return text
         .toString()                 // Cast to string (optional)
         .normalize('NFKD')          // The normalize() using NFKD method returns the Unicode Normalization Form of a given string.
         .toLowerCase()              // Convert the string to lowercase letters
         .trim()                     // Remove whitespace from both sides of a string (optional)
-        .replace(/\s+/g, '-')       // Replace spaces with -
+        .replace(/\s+/g, '-')       // Replace spaces with hyphen
         .replace(/[^\w\-]+/g, '')   // Remove all non-word chars
-        .replace(/\-\-+/g, '-')     // Replace multiple - with single -
-        .replace(/\-$/g, '');       // Remove trailing -
+        .replace(/\-\-+/g, '-')     // Replace multiple hyphen with single hyphen
+        .replace(/(^\-|\-$)/g, ''); // Remove leading or trailing hyphen
 }
 
 /**
